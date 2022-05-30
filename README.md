9906e7f4c259f16f8dbfa72cb9d2df79079a528c
ltoppyl <toppyajibutu@gmail.com>
2022-05-30 22:39:15 +0900

    feat: セットアップ
    基本的なレイアウト？の作成

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ohisama-sum-web
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <p>App</p>;
+};
+
+export default Home;
