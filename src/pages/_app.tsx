c553c46eca825d4a2924000816fe0cfcb8af4c18
ltoppyl <toppyajibutu@gmail.com>
2022-06-04 22:00:09 +0900

    feat: chakra ui を適用させる設置の追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from '@chakra-ui/react'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   );
 };
