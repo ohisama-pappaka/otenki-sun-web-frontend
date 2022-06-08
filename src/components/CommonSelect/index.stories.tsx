505a8fb3c578a5c79c4d794670a2bdf42d83531f
ltoppyl <toppyajibutu@gmail.com>
2022-06-08 21:32:15 +0900

    feat: 共通で使用できる select コンポーネントの作成

---
+++ src/components/CommonSelect/index.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, Story } from "@storybook/react/types-6-0";
+import { CommonSelect } from "./index";
+
+export default {
+  component: CommonSelect,
+  title: "tutorial/CommonSelect",
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
+} as Meta;
+
+const Template: Story = ({ placeholderData, selectDataList }) => (
+  <CommonSelect
+    placeholderData={placeholderData}
+    selectDataList={selectDataList}
+  />
+);
+
+export const commonSelect = Template.bind({});
+commonSelect.args = {
+  placeholderData: "placeholder",
+  selectDataList: ["Option1", "Option2", "Option3"],
+};
