3755dde73e9bf601ef748f2c25684cfb4049dd9d
ltoppyl <toppyajibutu@gmail.com>
2022-06-16 12:05:15 +0900

    refactor; チュートリアル用のコンポーネントの削除

--- src/components/tutorial/Headline/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-import React from "react";
-
-type Props = {
-  headingSize: "4xl" | "3xl" | "2xl" | "xl";
-};
-export const Headline = ({ headingSize }: Props) => {
-  return <Heading size={headingSize}>Headline</Heading>;
-};
